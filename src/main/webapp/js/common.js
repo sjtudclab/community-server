1c06c9cca1aa96b45c3afeb6a4c44fe63bfd017b
Applex <alex.nantong@gmail.com>
2015-03-16 15:56:50 +0800

    Add common.js which contains function currentUser() and currentUserId();

---
+++ src/main/webapp/js/common.js
@@ -0,0 +1,49 @@
+(function($) {
+	setCookie = function(name,value,days) {
+	    if (days) {
+	        var date = new Date();
+	        date.setTime(date.getTime()+(days*24*60*60*1000));
+	        var expires = "; expires="+date.toGMTString();
+	    }
+	    else var expires = "";
+	    document.cookie = name+"="+value+expires+"; path=/";
+	}
+
+	getCookie = function(name) {
+	    var nameEQ = name + "=";
+	    var ca = document.cookie.split(';');
+	    for(var i=0;i < ca.length;i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+	        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	    }
+	    return null;
+	}
+
+	eraseCookie = function(name) {
+	    createCookie(name,"",-1);
+	}
+
+	saveUser = function(user) {
+		setCookie("csUser", JSON.stringify(user));
+	}
+	
+	currentUser = function() {
+		var userString = getCookie("csUser");
+		try {
+			return JSON.parse(userString);
+		} catch (e) {
+			return null;
+		}
+	}
+	
+	currentUserId = function() {
+		var cu = currentUser();
+		if (cu == null) {
+			window.location = 'metrologin.html';
+			return 0;
+		}
+		return typeof cu.id == "undefined" ? 0 : cu.id;
+	}
+	
+})(jQuery);
