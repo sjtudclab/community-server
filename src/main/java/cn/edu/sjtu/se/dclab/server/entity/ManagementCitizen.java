36a86fcfd031c230101f6793d3f4e1fd99fa5f02
francis <huiyi.li@outlook.com>
2015-03-29 16:12:02 +0800

    Finish management/service citizen mapper

--- src/main/java/cn/edu/sjtu/se/dclab/server/entity/ManagementCitizen.java
+++ src/main/java/cn/edu/sjtu/se/dclab/server/entity/ManagementCitizen.java
@@ -14,4 +14,75 @@ public class ManagementCitizen {
     private String phone;
     private User user;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentificationType() {
+        return identificationType;
+    }
+
+    public void setIdentificationType(String identificationType) {
+        this.identificationType = identificationType;
+    }
+
+    public String getIdentificationValue() {
+        return identificationValue;
+    }
+
+    public void setIdentificationValue(String identificationValue) {
+        this.identificationValue = identificationValue;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/cn/edu/sjtu/se/dclab/server/entity/ServiceCitizen.java
+++ src/main/java/cn/edu/sjtu/se/dclab/server/entity/ServiceCitizen.java
@@ -4,4 +4,94 @@ package cn.edu.sjtu.se.dclab.server.entity;
  * Created by Huiyi on 2015/3/29.
  */
 public class ServiceCitizen {
+    private long id;
+    private String name;
+    private String identificationType;
+    private String identificationValue;
+    private String gender;
+    private int age;
+    private int status;
+    private String phone;
+    private String notes;
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentificationType() {
+        return identificationType;
+    }
+
+    public void setIdentificationType(String identificationType) {
+        this.identificationType = identificationType;
+    }
+
+    public String getIdentificationValue() {
+        return identificationValue;
+    }
+
+    public void setIdentificationValue(String identificationValue) {
+        this.identificationValue = identificationValue;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
