4c78837278a4cfef875c18ebde1ce7a51d256b75
francis <huiyi.li@outlook.com>
2015-04-02 10:30:39 +0800

    add friend_request page.

---
+++ src/main/java/cn/edu/sjtu/se/dclab/server/controller/basic/NeighborController.java
@@ -0,0 +1,18 @@
+package cn.edu.sjtu.se.dclab.server.controller.basic;
+
+import cn.edu.sjtu.se.dclab.server.common.Constants;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Huiyi on 2015/4/2.
+ */
+@Controller
+@RequestMapping(value = "resident/neighbor")
+public class NeighborController {
+
+    @RequestMapping("friend/request")
+    public String friendRequest() {
+        return "friend_request";
+    }
+}
