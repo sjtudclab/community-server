0ef81d7d9664c1fffcba832c0e19e0a360df708d
wbear <946010351@qq.com>
2015-04-02 15:20:44 +0800

    fix small bug

--- src/main/java/cn/edu/sjtu/se/dclab/server/controller/rest/GroupController.java
+++ src/main/java/cn/edu/sjtu/se/dclab/server/controller/rest/GroupController.java
@@ -1,9 +1,14 @@
 package cn.edu.sjtu.se.dclab.server.controller.rest;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.edu.sjtu.se.dclab.server.common.Constants;
+import cn.edu.sjtu.se.dclab.server.common.Result;
 
 /**
  *2015年4月2日 下午3:06:01
@@ -13,5 +18,21 @@ import cn.edu.sjtu.se.dclab.server.common.Constants;
 @RequestMapping(value = Constants.REST + "/groups")
 public class GroupController {
 	
+	@RequestMapping(value = "0/users/{userId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String getGroups(@PathVariable long userId){
+		return "";
+	}
 	
+	@RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String createGroup(){
+		return Result.SUCCESS;
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String addIntoGroup(){
+		return Result.SUCCESS;
+	}
 }
---
+++ src/main/java/cn/edu/sjtu/se/dclab/server/util/DataUtil.java
@@ -0,0 +1,8 @@
+package cn.edu.sjtu.se.dclab.server.util;
+/**
+ *2015年4月2日 下午3:13:42
+ *@author changyi yuan
+ */
+public class DataUtil {
+
+}
