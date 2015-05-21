6831b9b89d43b23122144e2dcf4987cc3f091d81
manchesterhui <chenhui46547@sina.com>
2015-05-21 18:19:51 +0800

    finish the js api for tenement management.

--- src/main/webapp/WEB-INF/statics/js/chen/repair_manage.js
+++ src/main/webapp/WEB-INF/statics/js/chen/repair_manage.js
@@ -1,3 +1,7 @@
+/* api for tenement management
+ * get data from server
+ */
+
 function get_all_serve_id(success, failure){
 	$.ajax({
 		url:getUrl("rest/tenement/serving/application/all"),
@@ -44,6 +48,71 @@ function unpass_serviceman(serve_id, success, failure){
 		type:"post",
 		dataType:"json",
 		success:function(){
+			if (typeof success == "function") {
+				success(obj);
+			}
+		}
+	});
+};
+
+function get_comment_order_id(success, failure){
+	$.ajax({
+		url:getUrl("rest/tenement/all/order/commented"),
+		type:"get",
+		dataType:"json",
+		success:function(obj){
+			if (typeof success == "function") {
+				success(obj);
+			}
+		}
+	});
+};
+
+function get_selected_comment(order_id, success, failure){
+	$.ajax({
+		url:getUrl("rest/tenement/order/comment/"+order_id),
+		type:"get",
+		dataType:"json",
+		success:function(obj){
+			if (typeof success == "function") {
+				success(obj);
+			}
+		}
+	});
+};
+
+function get_all_order_id(success, failure){
+	$.ajax({
+		url:getUrl("rest/tenement/all/order"),
+		type:"get",
+		dataType:"json",
+		success:function(obj){
+			if (typeof success == "function") {
+				success(obj);
+			}
+		}
+	});
+};
+
+function get_selected_order(order_id, success, failure){
+	$.ajax({
+		url:getUrl("rest/tenement/one/order/"+order_id),
+		type:"get",
+		dataType:"json",
+		success:function(obj){
+			if (typeof success == "function") {
+				success(obj);
+			}
+		}
+	});
+};
+
+function get_serviceman_info(success, failure){
+	$.ajax({
+		url:getUrl("rest/tenement/manage/serviceman/all"),
+		type:"get",
+		dataType:"json",
+		success:function(obj){
 			if (typeof success == "function") {
 				success(obj);
 			}
