18881741b0af38bc34a0499fec877d6765b7b05f
manchesterhui <chenhui46547@sina.com>
2015-05-24 14:27:48 +0800

    1. put market admin api in market_admin.js 2. finish JS API for market admin.

--- src/main/webapp/WEB-INF/statics/js/chen/market_admin.js
+++ src/main/webapp/WEB-INF/statics/js/chen/market_admin.js
@@ -1,3 +1,65 @@
 /* JS API for market resident
  * get and send data
  */
+
+function get_unchecked_data(success, failure) {
+	$.ajax({
+		url:getUrl("rest/SecondGoods/stage/1"),
+		type:"get",
+		dataType:"json",
+		success:function(obj){
+			if (typeof success == "function") {
+				success(obj);
+			}
+		},
+		error:function(){
+			alert("获取待审核物品信息失败");
+		}
+	});
+};
+
+function get_checked_data(success, failure) {
+	$.ajax({
+		url:getUrl("rest/SecondGoods/stage/2"),
+		type:"get",
+		dataType:"json",
+		success:function(obj){
+			if (typeof success == "function") {
+				success(obj);
+			}
+		},
+		error:function(){
+			alert("获取已审核物品信息失败");
+		}
+	});
+};
+
+function check_pass(goods_id) {
+	$.ajax({
+		url:getUrl("rest/SecondGoods/stage/update"),
+		type:"get",
+		//dataType:"json",
+		data:{goodsID:goods_id, stage:"2"},
+		success:function(data){
+			console.log(data);
+		},
+		error:function(){
+			alert("审核物品失败");
+		}
+	});
+};
+
+function check_unpass(goods_id) {
+	$.ajax({
+		url:getUrl("rest/SecondGoods/stage/update"),
+		type:"get",
+		//dataType:"json",
+		data:{goodsID:goods_id, stage:"3"},
+		success:function(data){
+			console.log(data);
+		},
+		error:function(){
+			alert("审核物品失败");
+		}
+	});
+};
